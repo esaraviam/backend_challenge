eae475e3b194a9aeab024222f91c311bc0e28c49
Luis Esteban Saravia M <esteban.saravia@millamanque.cl>
2024-09-16 16:31:17 -0300

    adding init files

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
+
+import cors from 'cors';
 import express from 'express';
 
-const app = express();
+
 const port = 3000;
+const app = express();
+
+app.use(cors())
 
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
