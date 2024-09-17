bc42ac1e908c32a20429ddbc3edae28d7f5593b2
Luis Esteban Saravia M <esteban.saravia@millamanque.cl>
2024-09-17 16:35:10 -0300

    adding nodemon

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,11 @@ app.use(cors())
 
 app.use(express.json());
 
+
+app.get("/products" , (req, res) => {
+  res.status(200).send("Hello World!");
+})
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 })
